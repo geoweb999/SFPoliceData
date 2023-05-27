ff87807fc5ba001dfa2f1f1236eb5b0d350c6770
geoweb999 <55800507+geoweb999@users.noreply.github.com>
2023-05-27 14:56:08 -0700

    Added graph

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -13,49 +21,100 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         String csvFile = "Police_Department_Incident_Reports__2018_to_Present.csv";
-        List<Incident> incidents = new ArrayList<Incident>();
 
         try (FileReader fileReader = new FileReader(csvFile);
              CSVParser csvParser = new CSVParser(fileReader, CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
 
-            // Get the headers from the CSV file
-            String[] headers = csvParser.getHeaderMap().keySet().toArray(new String[0]);
+            // Create a list to store Incident objects
+            List<Incident> incidents = csvParser.getRecords().stream()
+                    .map(Main::createIncidentFromRecord)
+                    .toList();
 
-            // Find the indices of the required fields
-            Map<String, Integer> fieldIndices = getFieldIndices(headers);
-
-            // Process each record of the CSV file
-            for (CSVRecord record : csvParser) {
-                // Create an Incident object from the CSV record
-                Incident incident = createIncidentFromRecord(record);
-
-                // Add the Incident object to the list
-                incidents.add(incident);
+            for (Incident incident : incidents) {
+                if (incident.getIncidentSubcategory().equals("Other")) {
+                    incident.setIncidentSubcategory("Other: " + incident.getIncidentDescription());
+                } else if (incident.getIncidentCategory().equals("Burglary")) {
+                    incident.setIncidentSubcategory("Burglary: " + incident.getIncidentDescription());
+                }
             }
 
-            System.out.println(incidents.size());
+            // Count Incidents by Incident Category and Incident Year using a stream
+            Map<String, TreeMap<String, Long>> countBySubCategoryAndYear = incidents.stream()
+                    .collect(Collectors.groupingBy(Incident::getIncidentSubcategory,
+                            TreeMap::new,
+                            Collectors.groupingBy(Incident::getIncidentYear, TreeMap::new, Collectors.counting())));
 
-            // Calculate count of incidents per Incident Category for each year using TreeMap
-            TreeMap<Integer, TreeMap<String, Long>> incidentCountMap = createIncidentCountMap(csvParser, fieldIndices);
+            // Print the count of Incidents by Incident Category and Incident Year (sorted)
+            for (Map.Entry<String, TreeMap<String, Long>> subCategoryEntry : countBySubCategoryAndYear.entrySet()) {
+                String category = subCategoryEntry.getKey();
+                TreeMap<String, Long> yearCountMap = subCategoryEntry.getValue();
 
-            // Print the count of incidents per Incident Category for each year
-            for (Map.Entry<Integer, TreeMap<String, Long>> entry : incidentCountMap.entrySet()) {
-                int year = entry.getKey();
-                TreeMap<String, Long> categoryCountMap = entry.getValue();
-
-                System.out.println("Year: " + year);
-                for (Map.Entry<String, Long> categoryEntry : categoryCountMap.entrySet()) {
-                    String category = categoryEntry.getKey();
-                    long count = categoryEntry.getValue();
-                    System.out.printf("    Incident Category: %-20s  Incident Count: %d%n", category, count);
+                System.out.println("Incident Category: " + category);
+                for (Map.Entry<String, Long> yearEntry : yearCountMap.entrySet()) {
+                    String year = yearEntry.getKey();
+                    long count = yearEntry.getValue();
+                    System.out.printf("    Incident Year: %4s  Incident Count: %d%n", year, count);
                 }
                 System.out.println();
+
+                CategoryDataset dataset = createDataset(incidents);
+
+                // Create the bar chart
+                JFreeChart chart = createBarChart(dataset);
+
+                // Save the chart to a PNG image file
+                saveChartAsPNG(chart, "incidents_per_year_chart.png");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private static CategoryDataset createDataset(List<Incident> incidents) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        // Filter incidents where Incident Category is "Burglary"
+        List<Incident> filteredIncidents = incidents.stream()
+                .filter(incident -> "Burglary".equals(incident.getIncidentCategory()))
+                .toList();
+
+        // Count incidents per year
+        TreeMap<String, Long> incidentsPerYear = filteredIncidents.stream()
+                .collect(Collectors.groupingBy(Incident::getIncidentYear, TreeMap::new, Collectors.counting()));
+
+        // Add incident counts to the dataset
+        for (Map.Entry<String, Long> entry : incidentsPerYear.entrySet()) {
+            String year = entry.getKey();
+            Long count = entry.getValue();
+            dataset.addValue(count, "Burglary", year);
+        }
+
+        return dataset;
+    }
+
+    private static JFreeChart createBarChart(CategoryDataset dataset) {
+        JFreeChart chart = ChartFactory.createBarChart(
+                "Incidents per Year (Category: Burglary)",
+                "Year",
+                "Incident Count",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        return chart;
+    }
+
+    private static void saveChartAsPNG(JFreeChart chart, String filename) {
+        try {
+            ChartUtilities.saveChartAsPNG(new File(filename), chart, 800, 600);
+            System.out.println("Chart saved as " + filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     private static Map<String, Integer> getFieldIndices(String[] headers) {
         Map<String, Integer> fieldIndices = new HashMap<>();
         for (int i = 0; i < headers.length; i++) {
